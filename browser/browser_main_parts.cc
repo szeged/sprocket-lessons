0c242a6a1a2235f46837fb3e4cd8e43caa210041
Attila Dusnoki <dati91@gmail.com>
2016-01-15 09:40:36 +0100

    Command Line

--- browser/browser_main_parts.cc
+++ browser/browser_main_parts.cc
@@ -8,7 +8,26 @@
 
 #include "sprocket/browser/window.h"
 #include "sprocket/browser/web_contents_delegate.h"
+#include "net/base/filename_util.h"
 #include "ui/base/ime/input_method_initializer.h"
+
+namespace {
+
+GURL GetStartupURL() {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  const base::CommandLine::StringVector& args = command_line->GetArgs();
+
+  if (args.empty())
+    return GURL("https://www.google.com/");
+
+  GURL url(args[0]);
+  if (url.is_valid() && url.has_scheme())
+    return url;
+
+  return net::FilePathToFileURL(base::FilePath(args[0]));
+}
+
+}
 
 SprocketBrowserMainParts::SprocketBrowserMainParts(
     const content::MainFunctionParams& parameters) {
@@ -29,7 +48,7 @@ void SprocketBrowserMainParts::PreMainMessageLoopRun() {
   SprocketWindow* window = SprocketWindow::CreateNewWindow(gfx::Size());
   SprocketWebContentsDelegate::CreateSprocketWebContentsDelegate(window,
                                                  browser_context(),
-                                                 GURL("https://www.google.com/"),
+                                                 GetStartupURL(),
                                                  gfx::Size());
 }
 
